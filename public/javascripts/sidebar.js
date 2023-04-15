d68087e03ac3a837a7146bdd75b8df031c5fa9a0
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-04-15 20:45:50 +0100

    Header e Sidebar Completo

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -1,12 +1,22 @@
-function showOnlyIcon() {
-    const ckToggleMenu = document.getElementById('ckToggleMenu');
-    if (ckToggleMenu.checked) {
-        const textMenuElement = document.querySelector('.menu-text');
-        textMenuElement.forEach((element) => {
-            element.style.display = 'none';
-        });
-    }
+const menuItem = document.querySelectorAll('.item-menu');
+
+function selectItem() {//Seleciona o item do menu activo 
+    menuItem.forEach((item) => {
+        item.classList.remove('active-item');
+    });
+
+    this.classList.add('active-item');//elemento que chama a function
 }
 
-const sidebarBtn = document.getElementById('sidebar-btn');
-sidebarBtn.addEventListener('click', showOnlyIcon);
+menuItem.forEach((item) => {
+    item.addEventListener('click', selectItem);
+});
+
+const btnMenu = document.querySelector('#btn-menu');
+const menuLateral = document.querySelector('.menu-lateral');
+
+function abrirFecharMenu() {//expande o menu lateral
+    menuLateral.classList.toggle('expandido');//add e remove a classe dinamicamente
+}
+
+btnMenu.addEventListener('click', abrirFecharMenu);
