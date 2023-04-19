f8d392fa98e3f11787f542eff041dee20f355f5c
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-04-19 22:05:42 +0100

    Finalizando a página de materiais

--- public/javascripts/material.js
+++ public/javascripts/material.js
@@ -10,5 +10,32 @@ function abrirModalNovo() {
     });
 }
 
+function abrirModalEditar() {
+    const modalEditar = document.querySelector('#modal-editar-material');
+
+    modalEditar.classList.add('is-active');
+
+    modalEditar.addEventListener('click', (evento) => {
+        if (evento.target.id == 'close' || evento.target.id == 'modal-editar-material') {
+            modalEditar.classList.remove('is-active');
+        }
+    });
+}
+
+function excluirRegisto() {
+    confirm("Tens a certeza que deseja excluir o MATERIAL?");
+}
+
+function excluirTodosRegistos() {
+    confirm("Esta acção apagará todos os registos, Confirmar?");
+}
+
 const btnNovo = document.querySelector('#btn-novo');
 btnNovo.addEventListener('click', abrirModalNovo);
+
+
+const btnEditar = document.querySelector('#btnEditar');
+btnEditar.addEventListener('click', abrirModalEditar);
+
+document.querySelector('#btnExcluir').addEventListener('click', excluirRegisto);
+document.querySelector('#btnLimparGrid').addEventListener('click', excluirTodosRegistos);
