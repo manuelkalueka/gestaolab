0490a7de398834009444603575d760b951d681a5
Manuel Kalueka <pedrokalueca@gmail.com>
2023-05-11 17:56:25 +0100

    Testando as sessões

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const TITULO_PAGE = 'Dashboard';
 
 router.get('/dashboard', (req, res, next) => {
     if (req.session.usuario) {
-        res.render('dashboard', { title: TITULO_PAGE, userLogado: req.session.usuario });
+        res.render('dashboard', { title: TITULO_PAGE});
     } else {
         res.redirect('/login');
     }
