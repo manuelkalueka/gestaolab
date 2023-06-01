d3b86fa903d7db4216891a0bcbda878b03f7bbcf
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-06-01 20:13:02 +0100

    Encriptacao de senha

--- passport.js
+++ passport.js
@@ -1,31 +1,36 @@
+const bcrypt = require('bcryptjs');
 var database = require('./database');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy((username,password,done)=>{
+passport.use(new LocalStrategy((username, password, done) => {
 
   database("usuarios")
-  .where("username", username)
-  .first()
-  .then((user)=>{
-    if(!user || user.password !== password){
-      console.log(user);
-        return done(null, false);
-    }
-    done(null, user)
+    .where("username", username)
+    .first()
+    .then((user) => {
+      // comparando as senhas
+      const isValid = bcrypt.compareSync(password, user.password);
 
-  }, done);
+      if (!user || !isValid) {
+        console.log(user);
+        return done(null, false);//usuario nao encontrado
+      }
+
+      done(null, user)
+
+    }, done);
 }));
 
-passport.serializeUser((user, done)=>{
+passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done)=>{
+passport.deserializeUser((id, done) => {
   database("usuarios")
-  .where("id", id)
-  .first()
-  .then((user)=>{
-    done(null, user)
-  },done);
+    .where("id", id)
+    .first()
+    .then((user) => {
+      done(null, user)
+    }, done);
 })
