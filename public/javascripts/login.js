33bcbd7078dbdbc6fbb6a7410f2769b3a5e89b68
Manuel Kalueka <109039442+manuelkalueka@users.noreply.github.com>
2023-06-26 22:51:45 +0100

    Paginacao de usuarios

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,7 @@
 function validarSenha() {
     const userNameElement = document.querySelector('#username');
     const senhaElement = document.querySelector('#password');
-    const alertMessage = document.getElementsByClassName('message-alert')[0];
+    const alertMessage = document.getElementsByClassName('message-error')[0];
 
     let username = userNameElement.value;
     let senha = senhaElement.value;
@@ -9,21 +9,23 @@ function validarSenha() {
     if (username == '' || senha == '') {
         event.preventDefault();
 
-        alertMessage.classList.remove('hide');
-        alertMessage.classList.add('alert-err')
-
         userNameElement.focus();
         return;
     }
 }
 
-document.querySelector('#senha').addEventListener('focus', () => {
-    document.getElementsByClassName('message-alert')[0].classList.add('hide');
+document.querySelector('#password').addEventListener('focus', () => {
+    document.getElementsByClassName('message-error')[0].classList.add('hide');
 });
 
 document.querySelector('#username').addEventListener('blur', () => {
-    document.getElementsByClassName('message-alert')[0].classList.add('hide');
+    document.getElementsByClassName('message-error')[0].classList.add('hide');
 });
 
 const btnEntrar = document.querySelector('#btnEntrar');
 btnEntrar.addEventListener('click', validarSenha);
+
+document.querySelector('.btnFogSenha').addEventListener('click', () => {
+    alert("Para repor sua senha contacte o Administrador do Sistema!");
+    return;
+})
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -7,15 +7,43 @@ const bcrypt = require('bcryptjs');
 
 router.get("/usuarios", async (req, res, next) => {
     try {
+        async function pagination(table) {
+            const numeroPagina = (req.query.pagina) || 1; //pagina actual
+            const registosPorPagina = 6;
+            const deslocamento = registosPorPagina * (parseInt(numeroPagina) - 1);
 
-        const usuarios = await database('usuarios')
-            .limit(6)
-            .orderBy('nome_completo', 'asc');
+            const dadosTable = await database(table)
+                .limit(6)
+                .offset(deslocamento)
+                .orderBy('nome_completo', 'asc');
+            const [total] = await database(table)
+                .count('*', { as: 'total' });
+            const resultado = Math.ceil(parseInt(total.total) / registosPorPagina);
+
+            const totalPaginas = resultado == 0 ? 1 : resultado;
+            return {
+                dadosTable,
+                totalPaginas,
+                numeroPagina,
+                deslocamento,
+            }
+        }
+
+        const dadosPaginados = await pagination('usuarios');
+
+        const paginas = {
+            actual: dadosPaginados.numeroPagina,
+            anterior: parseInt(dadosPaginados.numeroPagina) - 1 < 1 ? 1 : parseInt(dadosPaginados.numeroPagina) - 1,
+            proxima: parseInt(dadosPaginados.numeroPagina) + 1 > parseInt(dadosPaginados.totalPaginas) ? parseInt(dadosPaginados.totalPaginas) : parseInt(dadosPaginados.numeroPagina) + 1,
+            total: dadosPaginados.totalPaginas
+        }
+
         res.render("usuarios", {
             title: TITLE,
             sessao: req.session,
             usuario: req.user,
-            lista_usuarios: usuarios
+            lista_usuarios: dadosPaginados.dadosTable,
+            paginas: paginas,
         });
 
     } catch (erro) {
@@ -68,4 +96,84 @@ router.post('/usuarios/add', async (req, res, next) => {
         }, next)
 });
 
+router.get('/usuarios/edit-user/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const [usuarioEditar] = await database('usuarios')
+            .where('id', id);
+
+        if (!usuarioEditar) {
+            res.send(400);
+            return;
+        }
+        console.log(usuarioEditar);
+        res.render('edit-user', {
+            title: 'Editar Usuário',
+            usuario: req.user,
+            usuarioEditar: usuarioEditar
+        });
+    } catch (erro) {
+        console.log(erro.message);
+    }
+});
+
+router.put('/usuarios/edit-user/:id', async (req, res, next) => {
+    const reqDados = {
+        nome_completo: req.body.nome_completo,
+        bi: req.body.bi,
+        genero: req.body.genero,
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password),
+        tipo_conta: req.body.tipo_conta
+    }
+    let schema = yup.object({
+        nome_completo: yup.string().required("Preecha o campo nome"),
+        bi: yup.string().max(14),
+        genero: yup.string().required("Preecha o genero"),
+        username: yup.string().required("preecha o username").lowercase("O username deve ser minusculos"),
+        password: yup.string().required("Preecha a Palavra Passe").min(4)
+    });
+
+    if (!schema.isValid(req.body)) {
+
+        return res.status(400).render("edit-user",
+            {
+                title: "Editar Usuário",
+                message: {
+                    erro: true,
+                    texto: "erro",
+                },
+                sessao: req.session,
+                usuario: req.user
+            });
+    }
+
+    const { id } = req.params;
+    await database('usuarios')
+        .where('id', id)
+        .update(reqDados)
+        .then(result => {
+            if (result == 0) {
+                return res.send(400)
+            }
+
+            res.redirect('/usuarios');
+        }, next);
+});
+
+router.delete('/usuarios/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await database('usuarios')
+            .where('id', id)
+            .delete();
+
+        if (result === 0) {
+            return res.send(400);
+        }
+        res.redirect('/usuarios');
+    } catch (erro) {
+        console.log(erro.message);
+    }
+})
 module.exports = router;
