beefe58257e9b30d77ec9e1ad37372f984e3aa35
Manuel Kalueka <pedrokalueca@gmail.com>
2023-11-18 01:15:18 +0100

    Fix: Refazendo os relatórios

--- routes/report.js
+++ routes/report.js
@@ -1,277 +1,317 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const database = require('../database')
-const PDFPrinter = require('pdfmake');
-const fs = require('fs');
+const database = require("../database");
+const PDFPrinter = require("pdfmake");
+const fs = require("fs");
+const { getSelectedLab } = require("../middlewares/laboratorio");
 
-router.get('/report/materiais', async (request, response) => {
+router.get("/report/materiais", async (req, res) => {
+  //relatorio de materiais
+  try {
+    const [nomeLaboratorioSelecionado] = await database("laboratorios")
+      .where("id", getSelectedLab())
+      .select("nome");
+    console.log("object :>> ", nomeLaboratorioSelecionado);
+    const materiais = await database("materiais").where(
+      "laboratorio",
+      getSelectedLab()
+    );
+    const user = req.user.nome_completo;
 
-    try {
-        const materiais = await database('materiais');
-        const user = request.user.nome_completo;
+    const body = [];
+    const imagePath = "public/images/logo.png";
+    // console.log(imagePath)
+    const myLogo = fs.readFileSync(imagePath, "base64");
 
-        const body = [];
-        const imagePath = 'public/images/logo.png';
-        // console.log(imagePath)
-        const myLogo = fs.readFileSync(imagePath, 'base64');
+    for await (let material of materiais) {
+      const rows = [];
+      rows.push(material.nome);
+      rows.push(material.marca);
+      rows.push(material.estado);
 
-        for await (let material of materiais) {
-            const rows = [];
-            rows.push(material.nome)
-            rows.push(material.marca)
-            rows.push(material.estado)
-
-            body.push(rows);
-        }
-
-        const fonts = {
-            Times: {
-                normal: 'Times-Roman',
-                bold: 'Times-Bold',
-                italics: 'Times-Italic',
-                bolditalics: 'Times-BoldItalic'
-            }
-        };
-
-        const printer = new PDFPrinter(fonts) // configurar o pdf -- espera receber fonts
-
-        const docDefinition = {
-            defaultStyle: { font: 'Times', alignment: 'center' },
-            info: {
-                title: 'Relatório de Computadores no GestaoLab',
-                author: 'GestaoLab',
-                subject: 'Plano de Necessidades',
-            },
-            content: [
-                {
-                    image: `data:image/png;base64,${myLogo}`,
-                    width: 75,
-                },
-                {
-                    text: "Centro de Formação Tecnológico do Uíge\n\n\n".toUpperCase(),
-                    style: 'header'
-                },
-
-                {
-                    text: "Plano de Necessidades\n\n",
-                    style: 'header'
-                },
-
-                {
-                    table: {
-                        widths: ['40%', '30%', '30%'],
-                        heights: 10,
-                        body: [
-                            [{ text: "Designação", style: 'columnTitle' }, { text: "Marca", style: 'columnTitle' }, { text: "Estado", style: 'columnTitle' }], ...body]
-                    },
-                    style: 'text'
-                },
-                {
-                    text: `\n\nUíge, aos ${new Date().toLocaleDateString()}\n\n\n\n`,
-                    style: 'text'
-                },
-                {
-                    text: "O Responsável\n\n",
-                    style: 'text'
-                },
-                {
-                    text: `${user}`,
-                    style: 'textUSer'
-                },
-            ],
-            footer: [
-                {
-                    text: `Relatório Gerado pelo GestaoLab - Em: ${new Date().toLocaleString()}`, style: 'footer'
-                }
-            ],
-
-            styles: {
-                content: {
-                    alignment: "left"
-                },
-                header: {
-                    fontSize: 14,
-                    bold: true,
-                },
-                text: {
-                    fontSize: 12
-                },
-                textUSer: {
-                    fontSize: 12,
-                    italics: true
-                },
-                columnTitle: {
-                    bold: true,
-                    alignment: 'center'
-                },
-
-                footer: {
-                    fontSize: 10,
-                    italics: true,
-                    alignment: 'center'
-                },
-                imageStyle: {
-                }
-                // margin: {
-                //   margin: [2.5, 2, 2.5, 3]
-                // }
-            }
-        }
-
-        const pdfDoc = printer.createPdfKitDocument(docDefinition)// criar a definição do relatório
-        // pdfDoc.pipe(fs.createWriteStream("Relatorio.pdf")) Gravar o PDF
-
-        const chunks = []
-
-        pdfDoc.on('data', (chunk) => {// usar o chunk para iterar dentro do nosso relatorio
-            chunks.push(chunk)
-        })//pega o conteudo e trata ele
-
-        pdfDoc.end(); // termina a criação do relatório
-
-        pdfDoc.on('end', () => {
-            const result = Buffer.concat(chunks)
-            response.end(result);
-        })
-
-    } catch (error) {
-        console.log(error)
+      body.push(rows);
     }
-})
 
-router.get('/report/mesas', async (request, response) => {
+    const fonts = {
+      Times: {
+        normal: "Times-Roman",
+        bold: "Times-Bold",
+        italics: "Times-Italic",
+        bolditalics: "Times-BoldItalic",
+      },
+    };
 
-    try {
-        const mesas = await database('mesas');
-        const user = request.user.nome_completo;
+    const printer = new PDFPrinter(fonts); // configurar o pdf -- espera receber fonts
 
-        const body = [];
-        const imagePath = 'public/images/logo.png';
-        // console.log(imagePath)
-        const myLogo = fs.readFileSync(imagePath, 'base64');
+    const docDefinition = {
+      defaultStyle: { font: "Times", alignment: "center" },
+      info: {
+        title: "Relatório de Computadores no GestaoLab",
+        author: "GestaoLab",
+        subject: "Plano de Necessidades",
+      },
+      content: [
+        {
+          image: `data:image/png;base64,${myLogo}`,
+          width: 75,
+        },
+        {
+          text: "Centro de Formação Tecnológico do Uíge\n\n\n".toUpperCase(),
+          style: "header",
+        },
 
-        for await (let mesa of mesas) {
-            const rows = [];
-            rows.push(mesa.nome)
-            rows.push(mesa.num_maximo_pc)
-            rows.push(mesa.estado)
+        {
+          text:
+            "Plano de Necessidades para o " +
+            nomeLaboratorioSelecionado.nome +
+            "\n\n",
+          style: "header",
+        },
 
-            body.push(rows);
-        }
-
-        const fonts = {
-            Times: {
-                normal: 'Times-Roman',
-                bold: 'Times-Bold',
-                italics: 'Times-Italic',
-                bolditalics: 'Times-BoldItalic'
-            }
-        };
-
-        const printer = new PDFPrinter(fonts) // configurar o pdf -- espera receber fonts
-
-        const docDefinition = {
-            defaultStyle: { font: 'Times', alignment: 'center' },
-            info: {
-                title: 'Relatório de Computadores no GestaoLab',
-                author: 'GestaoLab',
-                subject: 'Plano de Necessidades',
-            },
-            content: [
-                {
-                    image: `data:image/png;base64,${myLogo}`,
-                    width: 75,
-                },
-                {
-                    text: "Centro de Formação Tecnológico do Uíge\n\n\n".toUpperCase(),
-                    style: 'header'
-                },
-
-                {
-                    text: "Plano de Necessidades\n\n",
-                    style: 'header'
-                },
-
-                {
-                    table: {
-                        widths: ['40%', '30%', '30%'],
-                        heights: 10,
-                        body: [
-                            [{ text: "Designação", style: 'columnTitle' }, { text: "Nº Máximo de Computador/Mesa", style: 'columnTitle' }, { text: "Estado", style: 'columnTitle' }], ...body]
-                    },
-                    style: 'text'
-                },
-                {
-                    text: `\n\nUíge, aos ${new Date().toLocaleDateString()}\n\n\n\n`,
-                    style: 'text'
-                },
-                {
-                    text: "O Responsável\n\n",
-                    style: 'text'
-                },
-                {
-                    text: `${user}`,
-                    style: 'textUSer'
-                },
+        {
+          table: {
+            widths: ["40%", "30%", "30%"],
+            heights: 10,
+            body: [
+              [
+                { text: "Designação", style: "columnTitle" },
+                { text: "Marca", style: "columnTitle" },
+                { text: "Estado", style: "columnTitle" },
+              ],
+              ...body,
             ],
-            footer: [
-                {
-                    text: `Relatório Gerado pelo GestaoLab - Em: ${new Date().toLocaleString()}`, style: 'footer'
-                }
-            ],
+          },
+          style: "text",
+        },
+        {
+          text: `\n\nUíge, aos ${new Date().toLocaleDateString()}\n\n\n\n`,
+          style: "text",
+        },
+        {
+          text: "O Responsável\n\n",
+          style: "text",
+        },
+        {
+          text: `${user}`,
+          style: "textUSer",
+        },
+      ],
+      footer: [
+        {
+          text: `Relatório Gerado pelo GestaoLab - Em: ${new Date().toLocaleString()}`,
+          style: "footer",
+        },
+      ],
 
-            styles: {
-                content: {
-                    alignment: "left"
-                },
-                header: {
-                    fontSize: 14,
-                    bold: true,
-                },
-                text: {
-                    fontSize: 12
-                },
-                textUSer: {
-                    fontSize: 12,
-                    italics: true
-                },
-                columnTitle: {
-                    bold: true,
-                    alignment: 'center'
-                },
+      styles: {
+        content: {
+          alignment: "left",
+        },
+        header: {
+          fontSize: 14,
+          bold: true,
+        },
+        text: {
+          fontSize: 12,
+          alignment: "center",
+        },
+        Number: { fontSize: 12, alignment: "right" },
+        textUSer: {
+          fontSize: 12,
+          italics: true,
+        },
+        columnTitle: {
+          bold: true,
+          alignment: "center",
+        },
 
-                footer: {
-                    fontSize: 10,
-                    italics: true,
-                    alignment: 'center'
-                },
-                imageStyle: {
-                }
-                // margin: {
-                //   margin: [2.5, 2, 2.5, 3]
-                // }
-            }
-        }
+        footer: {
+          fontSize: 10,
+          italics: true,
+          alignment: "center",
+        },
+        imageStyle: {},
+        // margin: {
+        //   margin: [2.5, 2, 2.5, 3]
+        // }
+      },
+    };
 
-        const pdfDoc = printer.createPdfKitDocument(docDefinition)// criar a definição do relatório
-        // pdfDoc.pipe(fs.createWriteStream("Relatorio.pdf")) Gravar o PDF
+    const pdfDoc = printer.createPdfKitDocument(docDefinition); // criar a definição do relatório
+    // pdfDoc.pipe(fs.createWriteStream("Relatorio.pdf")) Gravar o PDF
 
-        const chunks = []
+    const chunks = [];
 
-        pdfDoc.on('data', (chunk) => {// usar o chunk para iterar dentro do nosso relatorio
-            chunks.push(chunk)
-        })//pega o conteudo e trata ele
+    pdfDoc.on("data", (chunk) => {
+      // usar o chunk para iterar dentro do nosso relatorio
+      chunks.push(chunk);
+    }); //pega o conteudo e trata ele
 
-        pdfDoc.end(); // termina a criação do relatório
+    pdfDoc.end(); // termina a criação do relatório
 
-        pdfDoc.on('end', () => {
-            const result = Buffer.concat(chunks)
-            response.end(result);
-        })
+    pdfDoc.on("end", () => {
+      const result = Buffer.concat(chunks);
+      res.end(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    } catch (error) {
-        console.log(error)
+router.get("/report/mesas", async (req, res) => {
+  //relatorio de mesas
+  try {
+    const [nomeLaboratorioSelecionado] = await database("laboratorios")
+      .where("id", getSelectedLab())
+      .select("nome");
+
+    const mesas = await database("mesas").where(
+      "laboratorio",
+      getSelectedLab()
+    );
+    const user = req.user.nome_completo;
+
+    const body = [];
+    const imagePath = "public/images/logo.png";
+    // console.log(imagePath)
+    const myLogo = fs.readFileSync(imagePath, "base64");
+
+    for await (let mesa of mesas) {
+      const rows = [];
+      rows.push(mesa.nome);
+      rows.push(mesa.num_maximo_pc);
+      rows.push(mesa.estado);
+
+      body.push(rows);
     }
-})
+
+    const fonts = {
+      Times: {
+        normal: "Times-Roman",
+        bold: "Times-Bold",
+        italics: "Times-Italic",
+        bolditalics: "Times-BoldItalic",
+      },
+    };
+
+    const printer = new PDFPrinter(fonts); // configurar o pdf -- espera receber fonts
+
+    const docDefinition = {
+      defaultStyle: { font: "Times", alignment: "center" },
+      info: {
+        title: "Relatório de Computadores no GestaoLab",
+        author: "GestaoLab",
+        subject: "Plano de Necessidades",
+      },
+      content: [
+        {
+          image: `data:image/png;base64,${myLogo}`,
+          width: 75,
+        },
+        {
+          text: "Centro de Formação Tecnológico do Uíge\n\n\n".toUpperCase(),
+          style: "header",
+        },
+
+        {
+          text:
+            "Plano de Necessidades para o " +
+            nomeLaboratorioSelecionado.nome +
+            "\n\n",
+          style: "header",
+        },
+
+        {
+          table: {
+            widths: ["40%", "auto", "20%"],
+            heights: 10,
+            body: [
+              [
+                { text: "Designação", style: "columnTitle" },
+                {
+                  text: "Nº Máximo de Computador na Mesa",
+                  style: "columnTitle",
+                },
+                { text: "Estado", style: "columnTitle" },
+              ],
+              ...body,
+            ],
+          },
+          style: "text",
+        },
+        {
+          text: `\n\nUíge, aos ${new Date().toLocaleDateString()}\n\n\n\n`,
+          style: "text",
+        },
+        {
+          text: "O Responsável\n\n",
+          style: "text",
+        },
+        {
+          text: `${user}`,
+          style: "textUSer",
+        },
+      ],
+      footer: [
+        {
+          text: `Relatório Gerado pelo GestaoLab - Em: ${new Date().toLocaleString()}`,
+          style: "footer",
+        },
+      ],
+
+      styles: {
+        content: {
+          alignment: "left",
+        },
+        header: {
+          fontSize: 14,
+          bold: true,
+        },
+        text: {
+          fontSize: 12,
+          alignment: "center",
+        },
+        Number: { fontSize: 12, alignment: "right" },
+        textUSer: {
+          fontSize: 12,
+          italics: true,
+        },
+        columnTitle: {
+          bold: true,
+          alignment: "center",
+        },
+
+        footer: {
+          fontSize: 10,
+          italics: true,
+          alignment: "center",
+        },
+        imageStyle: {},
+        // margin: {
+        //   margin: [2.5, 2, 2.5, 3]
+        // }
+      },
+    };
+
+    const pdfDoc = printer.createPdfKitDocument(docDefinition); // criar a definição do relatório
+    // pdfDoc.pipe(fs.createWriteStream("Relatorio.pdf")) Gravar o PDF
+
+    const chunks = [];
+
+    pdfDoc.on("data", (chunk) => {
+      // usar o chunk para iterar dentro do nosso relatorio
+      chunks.push(chunk);
+    }); //pega o conteudo e trata ele
+
+    pdfDoc.end(); // termina a criação do relatório
+
+    pdfDoc.on("end", () => {
+      const result = Buffer.concat(chunks);
+      res.end(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
